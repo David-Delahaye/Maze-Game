df378eae4cf5c66f04d5eeb08034ede845e83c78
David Delahaye <delahaye3@hotmail.co.uk>
2020-05-24 16:47:57 +0100

    add game session properties

--- index.js
+++ index.js
@@ -13,14 +13,18 @@ let game = document.querySelector("#game");
 let container = document.querySelector(".container");
 let levelDisplay = document.querySelector("#level");
 let coinDisplay = document.querySelector("#coins");
+let timeDisplay = document.querySelector("#timer");
 let menu = document.querySelector(".menu");
 let menuHead = document.querySelector("#menu-head");
 let menuButton = document.querySelector("#menu-button");
-let viewWallsButton = document.querySelector("#view");
+let viewWallsButton = document.querySelector("#wallBreaker");
 let viewWallsButtonPressed = false;
-
+let timer = null;
+let timeLeft = 10;
 let level = localStorage.getItem('level') || 1;
 let coins = localStorage.getItem('coins') || 0;
+let startingSession = true;
+let active = false;
 
 viewWallsButton.addEventListener("click", () => {
   if (coins >= 5 && game1.enemies.length > 0 && viewWallsButtonPressed===false) {
@@ -33,21 +37,33 @@ viewWallsButton.addEventListener("click", () => {
   }
 });
 
+function startTimer(){
+timer = setInterval(()=>{
+console.log('tick');
+timeLeft--
+update();
+},1000)
+}
+
+
 const update = () => {
-  levelDisplay.innerHTML = "Level: " + level;
-  coinDisplay.innerHTML = "Coins: " + coins;
+  timeDisplay.innerHTML = "Time Left: " + timeLeft;
   localStorage.setItem('level', level);
   localStorage.setItem('coins', coins);
+  levelDisplay.innerHTML = "Level: " + localStorage.getItem('level', level);;
+  coinDisplay.innerHTML = "Coins: " + coins;
 };
 update();
 
-const newLevel = (reason) => {
+const nextLevel = (reason) => {
   game1 = new Game(game, level, game.clientWidth, game.clientHeight);
   game1.mazeBuild();
   game1.spritesBuild();
   game1.controlsBuild();
   game1.eventsBuild();
   viewWallsButtonPressed = false;
+  viewWallsButton.style.backgroundColor = "hsl(" + game1.hue + ", 40%,20%)";
+  update();
   console.log(reason);
 };
 
@@ -55,21 +71,36 @@ const clear = () => {
   game1.worldClear();
 };
 
-newLevel("start");
-
 const win = () => {
+  level++
   menu.classList.remove("hidden");
   menuHead.textContent = "Level " + level + " Complete";
+  menuButton.textContent = "Next Level";
   menuButton.style.backgroundColor = "hsl(" + game1.hue + ", 40%,30%)";
 };
 
 menuButton.addEventListener("click", () => {
+  if (startingSession){
+    clearInterval(timer);
+    timeLeft = 10;
+    startTimer();
+    menu.classList.add("hidden");
+    startingSession = false;
+  }else{
+    newLevel();
+  }
+});
+
+const newLevel = () =>{
+  clearInterval(timer);
+  timeLeft = 10;
+  startTimer();
   menu.classList.add("hidden");
   level++;
   update();
   clear();
-  newLevel("win");
-});
+  nextLevel("win");
+}
 
 const coinPickup = () => {
   coins++;
@@ -94,6 +125,8 @@ const reset = () => {
   newLevel("reset");
 };
 
+nextLevel('start');
+
 window.addEventListener("resize", debounce(reset, 1000));
 
 //stop scrolling
@@ -109,5 +142,3 @@ function preventMotion(event) {
 // container.style.width = window.innerWidth + 'px';
 // container.style.height = window.innerHeight + 'px';
 
-menuHead.textContent = "Maze Game";
-menu.classList.remove("hidden");
